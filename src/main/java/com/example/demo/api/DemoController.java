b6f4b71258062ff5f333a49fd6037dc3d0503e52
Timo Schäfer <timo.schaefer@gmail.com>
2024-07-23 22:42:52 +0200

    Init project

---
+++ src/main/java/com/example/demo/api/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.demo.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
